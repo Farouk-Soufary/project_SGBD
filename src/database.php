388ca0e5b7cfc3fb897775ae318097a1f9504692
ELALAOUI Mohamed_Seddiq <Mohamed_Seddiq.Elalaoui@bordeaux-inp.fr>
2022-11-18 09:51:22 +0100

    added functional interactive database

--- src/database.php
+++ src/database.php
@@ -99,11 +99,78 @@
             <div>
             <button class="button-table isActive" style="left: 10vw; background-color: rgb(43, 122, 120);" onclick="showTable(1);" title="Joueurs">Joueurs</button>
             </div>
+            
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">Prenom</th>
+                            <th style="width:10%;">Nom </th>
+                            <th style="width:10%;">Pseudonyme</th>
+                            <th style="width:10%;">Email</th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM JOUEURS');
+                        $rs->execute();
+                        $players = $rs->fetchAll();
+                        foreach ($players as $player) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $player['PRENOM_JOUEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $player['NOM_JOUEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $player['PSEUDONYME'];?></td >
+                                    <td style="width:10%;"><?php echo $player['ADRESSE_MAIL'];?></td >
+                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
 
         <div class="page-2" style="z-index = -1;">
             <div>
             <button class="button-table" style="left: 21vw; background-color:  rgb(40, 54, 61);" onclick="showTable(2);" title="Joueurs">Jeux</button>
+            </div>
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">Nom</th>
+                            <th style="width:10%;">Editeur </th>
+                            <th style="width:10%;">Date de parution</th>
+                            <th style="width:10%;">Durée</th>
+                            <th style="width:10%;">Type</th>
+                            <th style="width:10%;">Nombre de joueurs</th>
+                            <th style="width:10%;">Stand Alone</th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM JEUX');
+                        $rs->execute();
+                        $games = $rs->fetchAll();
+                        foreach ($games as $game) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $game['EDITEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $game['NOM_JEU'];?></td >
+                                    <td style="width:10%;"><?php echo $game['DATE_PARUTION'];?></td >
+                                    <td style="width:10%;"><?php echo $game['DUREE'];?></td >
+                                    <td style="width:10%;"><?php echo $game['TYPE_JEU'];?></td >
+                                    <td style="width:10%;"><?php echo $game['NOMBRE_JOUEURS'];?></td >
+                                    <td style="width:10%;"><?php echo $game['STAND_ALONE'];?></td >
+                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
             </div>
         </div>
 
@@ -111,11 +178,73 @@
             <div>
             <button class="button-table" style="left: 32vw; background-color:  rgb(118, 13, 34);" onclick="showTable(3);" title="Joueurs">Notes</button>
             </div>
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">ID</th>
+                            <th style="width:10%;">Commentaire </th>
+                            <th style="width:10%;">Date</th>
+                            <th style="width:10%;">Valeur</th>
+                            <th style="width:10%;">Nombre de joueurs</th>
+                            <th style="width:10%;">Nom du jeu</th>
+                            <th style="width:10%;">Pseudonyme</th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM NOTES');
+                        $rs->execute();
+                        $notes = $rs->fetchAll();
+                        foreach ($notes as $note) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $note['ID_NOTE'];?></td >
+                                    <td style="width:10%;"><?php echo $note['COMMENTAIRE'];?></td >
+                                    <td style="width:10%;"><?php echo $note['DATE_NOTE'];?></td >
+                                    <td style="width:10%;"><?php echo $note['VALEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $note['NOMBRE_JOUEURS'];?></td >
+                                    <td style="width:10%;"><?php echo $note['NOM_JEU'];?></td >
+                                    <td style="width:10%;"><?php echo $note['PSEUDONYME'];?></td >
+                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
 
         <div class="page-4" style="z-index = -1;">
             <div>
             <button class="button-table" style="left: 43vw; background-color:  olive;" onclick="showTable(4);" title="Joueurs">Mécaniques</button>
+            </div>
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">Jeu</th>
+                            <th style="width:10%;">Mécanique </th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM JEUX_MECANIQUES');
+                        $rs->execute();
+                        $mechanics = $rs->fetchAll();
+                        foreach ($mechanics as $mechanic) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $mechanic['NOM_JEU'];?></td >
+                                    <td style="width:10%;"><?php echo $mechanic['NOM_MECANIQUES'];?></td >
+                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
             </div>
         </div>
 
@@ -123,11 +252,62 @@
             <div>
             <button class="button-table" style="left: 54vw; background-color:  peru;" onclick="showTable(5);" title="Joueurs">Thèmes</button>
             </div>
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">Thèmes</th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM THEMES');
+                        $rs->execute();
+                        $themes = $rs->fetchAll();
+                        foreach ($themes as $theme) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $theme['NOM_THEME'];?></td >                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
 
         <div class="page-6" style="z-index = -1;">
             <div>
             <button class="button-table" style="left: 65vw; background-color:  rgb(0, 109, 109);" onclick="showTable(6);" title="Joueurs">Contributeurs</button>
+            </div>
+            <div class="consult-wrap" style="transform: translate(-30vw,-40vh); ">
+                <div class="consult-grid" style="overflow-y: scroll;">
+                <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                        <tr>
+                            <th style="width:10%;">ID</th>
+                            <th style="width:10%;">Nom </th>
+                            <th style="width:10%;">Prenom</th>
+                        </tr>
+                    </table>
+                    <?php
+                        $rs = $db->prepare('SELECT * FROM CONTRIBUTEURS');
+                        $rs->execute();
+                        $contributors = $rs->fetchAll();
+                        foreach ($contributors as $contributor) {
+                        ?>
+                        <table style="width:100%;">
+                                <tr>
+                                    <td style="width:10%;"><?php echo $contributor['ID_CONTRIBUTEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $contributor['NOM_CONTRIBUTEUR'];?></td >
+                                    <td style="width:10%;"><?php echo $contributor['PRENOM_CONTRIBUTEUR'];?></td >
+                                    
+                                </tr>
+                    </table>
+                        <?php
+                        }
+                    ?>
+                </div>
             </div>
         </div>
 
