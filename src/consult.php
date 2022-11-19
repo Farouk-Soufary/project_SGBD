977cf71a0a1e42ad3b4451a4e169eae84892bbfb
Farouk <farouksou-spiele2000@outlook.fr>
2022-11-19 11:28:35 +0100

    Consult 2 and 3 are now working

--- src/consult.php
+++ src/consult.php
@@ -16,27 +16,27 @@
 
         <div class="utilities-links">
             <div class="home-link">
-                <a href="index.php" class="text-link">Accueil</a> 
+                <a href="index.php" class="text-link">Accueil</a>
             </div>
 
             <div class="consult-link">
-                <a href="database.php" class="text-link">DataBase</a> 
+                <a href="database.php" class="text-link">DataBase</a>
             </div>
 
             <div class="consult-link">
-                <a href="consult.php" class="text-link">Consulter</a> 
+                <a href="consult.php" class="text-link">Consulter</a>
             </div>
 
             <div class="statistics-link">
-                <a href="statistics.php" class="text-link">Statistiques</a> 
+                <a href="statistics.php" class="text-link">Statistiques</a>
             </div>
 
             <div class="modify-link">
-                <a href="modify.php" class="text-link">Modifier</a> 
+                <a href="modify.php" class="text-link">Modifier</a>
             </div>
 
             <div class="about-link">
-                <a href="about.php" class="text-link">À propos</a> 
+                <a href="about.php" class="text-link">À propos</a>
             </div>
 
         </div>
@@ -73,8 +73,8 @@
 		    foreach ($themes as $theme) {
 		    ?>
             <form action="" method="post">
-		    <button class="test-bar" name="Theme" type="submit" ><?php echo $theme['NOM_THEME']; ?></button>
-            </form>  
+		    <button class="test-bar" name=<?php echo $theme['NOM_THEME'];?> type="submit" ><?php echo $theme['NOM_THEME']; ?></button>
+            </form>
 		    <?php
 		     }
 		     ?>
@@ -88,21 +88,36 @@
                     </table>
                     <?php
                     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-                        if (isset($_POST["Theme"])) {
-                               $rs = $db->prepare('SELECT * FROM THEMES');
+                      $rs = $db->prepare('SELECT * FROM THEMES');
+              		    $rs->execute();
+              		    $themes = $rs->fetchAll();
+              		    foreach ($themes as $theme) {
+                        if (isset($_POST[$theme['NOM_THEME']])) {
+                               $ttarget = $theme['NOM_THEME'];
+                               $rs = $db->prepare('SELECT J.* from JEUX J
+                               inner join NOTES N
+                               on J.NOM_JEU = N.NOM_JEU
+                               inner join JEUX_THEMES JT
+                               on J.NOM_JEU = JT.NOM_JEU
+                               inner join JEUX_MECANIQUES JM
+                               on J.NOM_JEU = JM.NOM_JEU
+                               where JT.NOM_THEME =:thetheme
+                               order by JM.NOM_MECANIQUES');
+                               $rs->bindParam('thetheme',$ttarget,PDO::PARAM_STR);
                                $rs->execute();
-                               $themes = $rs->fetchAll();
-                               foreach ($themes as $theme) {
+                               $games = $rs->fetchAll();
+                               foreach ($games as $game) {
                                ?>
                                <table style="width:100%;">
                                         <tr>
-                                            <td style="width:5%;"><?php echo $theme['NOM_THEME'];?></td >
+                                            <td style="width:5%;"><?php echo $game['NOM_JEU'];?></td>
                                         </tr>
                             </table>
                                <?php
                                 }
                         }
                     }
+                  }
                     ?>
                 </div>
             </div>
@@ -112,7 +127,7 @@
             <div class="consult-wrap">
                 <div class="title-grid">Commentaires d'un des jeux préférés du joueur 'PLAYER'</div>
                 <button class="choose-button">Choix du joueur</button>
-                
+
                 <div class="choose-bar">
                 <?php
 		        $db = new PDO(
@@ -126,32 +141,77 @@
                 foreach ($joueurs as $joueur) {
                 ?>
                 <form  action="" method="post">
-                <button class="test-bar" type=submit name="Player"><?php echo $joueur['NOM_JOUEUR']; ?></button>
+                <button class="test-bar" type=submit name=<?php echo $joueur['PSEUDONYME'];?> ><?php echo $joueur['PSEUDONYME']; ?></button>
                 </form>
                 <?php
                 }
                 ?>
               	</div>
-                <div class="consult-grid" style="overflow-y: scroll;">
                 <?php
-                    if ($_SERVER['REQUEST_METHOD'] === "POST") {
-                        if (isset($_POST["Player"])) {
-                               $rs = $db->prepare('SELECT * FROM JOUEURS');
-                               $rs->execute();
-                               $themes = $rs->fetchAll();
-                               foreach ($themes as $theme) {
-                               ?>
-                               <table style="width:100%;">
-                                        <tr>
-                                            <td style="width:5%;"><?php echo $theme['NOM_JOUEUR'];?></td >
-                                        </tr>
-                            </table>
-                               <?php
-                                }
-                        }
+                if ($_SERVER['REQUEST_METHOD'] === "POST") {
+                  $rs = $db->prepare('SELECT * FROM JOUEURS');
+                  $rs->execute();
+                  $joueurs = $rs->fetchAll();
+                  foreach($joueurs as $joueur){
+                    if (isset($_POST[$joueur['PSEUDONYME']])) {
+                      $player = $joueur['PSEUDONYME'];
+                      $rs = $db->prepare('SELECT distinct J.* from JEUX J
+                      inner join JEUX_MECANIQUES JM
+                      on JM.NOM_JEU = J.NOM_JEU
+                      inner join JOUEURS_MECANIQUES JOM
+                      on JOM.NOM_MECANIQUES = JM.NOM_MECANIQUES
+                      where JOM.PSEUDONYME =:playername');
+                      $rs->bindParam('playername',$player,PDO::PARAM_STR);
+                      $rs->execute();
+                      $games = $rs->fetchAll();
+                      foreach($games as $game){
+                      ?>
+                      <form  action="" method="post">
+                      <button class="test-bar" type=submit name=<?php echo $game['NOM_JEU'];?> ><?php echo $game['NOM_JEU']; ?></button>
+                      </form>
+                      <?php
+                      }
                     }
-                    ?>
-                </div>
+
+                  }?>
+
+
+                  <div class="consult-grid" style="overflow-y: scroll;">
+                  <table style="width:100%; border-radius-bottom: 2vh; background-color: rgb(0,0,0,.2); box-shadow: 0 0 4vh .1vh rgb(0,0,0,.4);">
+                          <tr>
+                                  <th style="width:30%;">COMMENTS</th>
+                          </tr>
+                      </table>
+                      <?php
+                  if ($_SERVER['REQUEST_METHOD'] === "POST") {
+                    $rs = $db->prepare('SELECT * FROM JEUX');
+                    $rs->execute();
+                    $games = $rs->fetchAll();
+                    foreach($games as $game){
+                      if (isset($_POST[$game['NOM_JEU']])) {
+                        $targetgame = $game['NOM_JEU'];
+                        $rs = $db->prepare('SELECT distinct N.* from NOTES N
+                        inner join JEUX J
+                        on J.NOM_JEU = N.NOM_JEU
+                        where J.NOM_JEU =:lejeu');
+                        $rs->bindParam('lejeu',$targetgame,PDO::PARAM_STR);
+                        $rs->execute();
+                        $lesnotes = $rs->fetchAll();
+                        foreach($lesnotes as $unenote){?>
+                          <table style="width:100%;">
+                                   <tr>
+                                      <td style="width:5%;"><?php echo $unenote['COMMENTAIRE'];?></td>
+                                   </tr>
+                          </table>
+                          <?php
+                        }
+                      }
+                  }
+                }
+              }
+                ?>
+              </div>
+              
             </div>
         </div>
 
@@ -159,7 +219,7 @@
             <div class="consult-wrap">
                 <div class="title-grid">Joueurs qui ont appréciés le commentaire 'ID' de 'PLAYER'</div>
                 <button class="choose-button">Choix du commentaire</button>
-                
+
                 <div class="choose-bar">
                     <?php
                     $db = new PDO(
@@ -176,7 +236,7 @@
                     <button class="test-bar" type=submit name="<?php echo $note['ID_NOTE']; ?>"><?php echo $note['ID_NOTE']; ?></button>
                     <?php
                     }
-                    ?>    
+                    ?>
                 </div>
 
                 <div class="consult-grid" style="overflow-y: scroll;">
